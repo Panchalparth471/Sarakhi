b0c84b1ae34ede91e4818b51e5e8c777743ac4ae
Zynox The Dev <116634521+Panchalparth471@users.noreply.github.com>
2024-01-27 12:03:34 +0530

    Update Product.js

--- src/Product.js
+++ src/Product.js
@@ -7,7 +7,7 @@ export default function Product(props) {
       <div>
         <p className="w-40 font-normal text-[10px] text-left text-gray-500 " >{props.description.split(" ").slice(0,10).join(" ") + "..." }</p>
       </div>
-      <div className="h-[180px]">
+      <div className="h-[80%]">
         <img src={`${props.image}`} className="h-full w-full"></img>
         </div>
         
